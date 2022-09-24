2e06c511f730f0cb8b1c13c730e20ad102fd84e7
Maks Macioszczyk <maksmacioszczyk@gmail.com>
2022-09-24 23:45:20 +0200

    Added testing and dataCollection scripts Added gui support for showing camera frame

--- src/main.py
+++ src/main.py
@@ -6,7 +6,11 @@ import math
 
 cap = cv2.VideoCapture(0)
 cap.open("http://192.168.0.19:8000/")
-detector = HandDetector(detectionCon=0.8, maxHands=2)
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+isGesture = False
+mariginError = 50
+
 
 x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
 y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
@@ -17,7 +21,7 @@ while True:
     # Get image frame
     success, img = cap.read()
     # Find the hand and its landmarks
-    hands, img = detector.findHands(img)  # with draw
+    hands = detector.findHands(img, draw=False)  # with draw
     # hands = detector.findHands(img, draw=False)  # without draw
 
     if hands:
@@ -28,7 +32,12 @@ while True:
         centerPoint1 = hand1['center']  # center of the hand cx,cy
         handType1 = hand1["type"]  # Handtype Left or Right
         fingers1 = detector.fingersUp(hand1)
-        
+          
+        if lmList1[8][0] - lmList1[4][0] <= mariginError and lmList1[8][1] - lmList1[4][1] <= mariginError and lmList1[8][2] - lmList1[4][2] <= mariginError:
+            isGesture = True
+        else:
+            isGesture = False
+            
         lmList = hands[0]['lmList']
         x, y, w, h = hands[0]['bbox']
         x1, y1, _ = lmList[5]
@@ -37,9 +46,18 @@ while True:
         distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
         A, B, C = coff
         distanceCM = A * distance ** 2 + B * distance + C
-        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x+5, y-10))
-
-        print(centerPoint1)
+        
+        fingers = detector.fingersUp(hands[0])
+        if fingers == [1, 1, 1, 1, 1]:
+            cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(centerPoint1[0])} Y:{int(centerPoint1[1])}', (x+5, y-10), border=2)
+        elif fingers == [1,1,0,1,1]:
+            cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(centerPoint1[0])} Y:{int(centerPoint1[1])}', (x+5, y-10))
+                
+        # if not isGesture:
+        #     cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(centerPoint1[0])} Y:{int(centerPoint1[1])}', (x+5, y-10), border=2)
+        # else:
+        #     cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(centerPoint1[0])} Y:{int(centerPoint1[1])}', (x+5, y-10))
+        
     # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
---
+++ src/utils/testing.py
@@ -0,0 +1,70 @@
+from cProfile import label
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+
+cap = cv2.VideoCapture()
+cap.open("http://192.168.0.19:8000/")
+detector = HandDetector(maxHands=1)
+classifier = Classifier("/home/wsl-ubuntu/robot_arm_controll/Model/keras_model.h5", "/home/wsl-ubuntu/robot_arm_controll/Model/labels.txt")
+
+offset = 20
+imgSize = 300
+
+positions_file = "/home/wsl-ubuntu/robot_arm_controll/positions.txt"
+
+labels = ["Zapisz", "Wskaznik", "Otworz", "Zamknij", ""]
+
+while True:
+    success, img = cap.read()
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        if labels[index] == labels[0]:
+            with open(positions_file, "a") as file:
+                file.write(str(hand['center']) + "\n")
+        
+        
+        cv2.rectangle(imgOutput, (x - offset, y - offset-50),
+                      (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x-offset, y-offset),
+                      (x + w+offset, y + h+offset), (255, 0, 255), 4)
+
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", imgOutput)
+    cv2.waitKey(1)
