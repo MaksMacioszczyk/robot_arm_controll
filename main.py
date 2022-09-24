f3a348d6bbef80afc35d00807421b8e2481111ba
Maks Macioszczyk <maksmacioszczyk@gmail.com>
2022-09-24 21:14:22 +0200

    Distance measurement

---
+++ main.py
@@ -0,0 +1,47 @@
+from cvzone.HandTrackingModule import HandDetector
+import cvzone
+import cv2
+import numpy as np
+import math
+
+cap = cv2.VideoCapture(0)
+cap.open("http://192.168.0.19:8000/")
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+coff = np.polyfit(x, y, 2)  # y = Ax^2 + Bx + C
+
+
+while True:
+    # Get image frame
+    success, img = cap.read()
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)  # with draw
+    # hands = detector.findHands(img, draw=False)  # without draw
+
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+        fingers1 = detector.fingersUp(hand1)
+        
+        lmList = hands[0]['lmList']
+        x, y, w, h = hands[0]['bbox']
+        x1, y1, _ = lmList[5]
+        x2, y2, _ = lmList[17]
+ 
+        distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
+        A, B, C = coff
+        distanceCM = A * distance ** 2 + B * distance + C
+        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x+5, y-10))
+
+        print(centerPoint1)
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
