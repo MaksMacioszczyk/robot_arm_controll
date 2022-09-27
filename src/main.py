cc5e694fe87226bef3ed0347b03edd1f3ac2e825
Maks Macioszczyk <maksmacioszczyk@gmail.com>
2022-09-27 20:19:29 +0200

    Added inverse kinematics

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 from cvzone.HandTrackingModule import HandDetector
 import cvzone
 import cv2
@@ -5,58 +6,63 @@ import numpy as np
 import math
 import serial
 
+
+##Serial Port Init##
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = 'COM4'
 ser.timeout = 1
-
-cap = cv2.VideoCapture(0)
-
-detector = HandDetector(detectionCon=0.8, maxHands=1)
 ser.open()
-isOn = False
 ser.write(b'off')
+
+##Camera init##
+camera = cv2.VideoCapture(0)
+camera_width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+camera_height =  camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+camera_suspension_height = 50
+
+##Variables##
+robot_max_range_CM = [10,10,10] # [X, Y, Z]
+robot_arm_lengths = [20,15,10] # [A1, A2, A3]
+isOn = False
 isGesture = False
-mariginError = 50
 
-
+##Distance shenanigans##
 x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
 y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 coff = np.polyfit(x, y, 2)  # y = Ax^2 + Bx + C
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 
 
+####MAIN LOOP####
 while True:
-    # Get image frame
-    success, img = cap.read()
-    # Find the hand and its landmarks
-    hands = detector.findHands(img, draw=False)  # with draw
-    # hands = detector.findHands(img, draw=False)  # without draw
+    success, img = camera.read()
+    hands = detector.findHands(img, draw=False)  
 
     if hands:
-        # Hand 1
+        #Reading from mediapipe output#
         hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # List of 21 Landmark points
-        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
-        centerPoint1 = hand1['center']  # center of the hand cx,cy
-        handType1 = hand1["type"]  # Handtype Left or Right
-        fingers1 = detector.fingersUp(hand1)
-            
+        lmList = hand1["lmList"] 
+        bbox1 = hand1["bbox"]   
         lmList = hands[0]['lmList']
         x, y, w, h = hands[0]['bbox']
         x1, y1, _ = lmList[5]
         x2, y2, _ = lmList[17]
- 
+        #####################
+        
+        #Measuring distance#
         distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
         A, B, C = coff
         distanceCM = A * distance ** 2 + B * distance + C
+        #####################
         
+        #Gestures recognition#
         fingers = detector.fingersUp(hands[0])
         if fingers == [1, 1, 1, 1, 1]:
-            cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(centerPoint1[0])} Y:{int(centerPoint1[1])}', (x+5, y-10), border=2)
+            cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(lmList[8][0])} Y:{int(lmList[8][1])}', (x+5, y-10), border=2)
             isGesture = False
         elif fingers == [1,1,0,1,1]:
-            cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(centerPoint1[0])} Y:{int(centerPoint1[1])}', (x+5, y-10), border=3)
-            
+            cvzone.putTextRect(img, f'{int(distanceCM)} cm X:{int(lmList[8][0])} Y:{int(lmList[8][1])}', (x+5, y-10), border=3)
             if not isGesture:
                 isGesture = True
                 if isOn:
@@ -66,12 +72,23 @@ while True:
                     send_to_Arduino = b"on"
                     isOn = True
                 ser.write(send_to_Arduino)
+        ##########################        
+        
+        #Counting robot X Y Z based on camera output#
+        robot_X = lmList[8][0]/camera_width * robot_max_range_CM[0]
+        robot_Y = (1 -lmList[8][1]/camera_height) * robot_max_range_CM[1]
+        robot_Z = (1 - distanceCM/camera_suspension_height) * robot_max_range_CM[2]
+        #################
 
+        #Inverse kinematics#
+        M = (robot_X**2 + robot_Y**2 - robot_arm_lengths[0]**2 - robot_arm_lengths[1]**2)/(2*robot_arm_lengths[0]*robot_arm_lengths[1])
+        fi2 = np.arctan((-np.sqrt(1-M**2))/(M))
+        fi1 = np.arctan(robot_Y/robot_X)-np.arctan((robot_max_range_CM[1]*np.sin(fi2))/(robot_max_range_CM[0]+robot_max_range_CM[1] * np.cos(fi2)))
+         
+        fi1_deg = np.rad2deg(fi1)
+        fi2_deg = np.rad2deg(fi2)
+        ###################
+        print(f'FI1:{fi1_deg} FI2:{fi2_deg} Z:{robot_Z}\n') # Print angles #          
     
-           
-    # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-cap.release()
-cv2.destroyAllWindows()
