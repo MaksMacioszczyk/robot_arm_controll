e46371e8d05abc69350519c617d8a54a48b161c5
Maks Macioszczyk <maksmacioszczyk@gmail.com>
2022-10-07 15:02:10 +0200

    Added comboBox

--- src/main.py
+++ src/main.py
@@ -18,17 +18,27 @@ class WindowApp:
     ##Qt variables##
     button_exit = window.button_exit
     button_camera = window.button_camera
+    button_add_to_path = window.button_add_to_path
     label_positions = window.label_positions
+    label_path = window.label_path
+    combo_items = window.combo_items
     ################    
+    
+    ##Variables##
+
     
     def __init__(self):
         ##Adding actions to buttons##
         self.button_exit.clicked.connect(sys.exit) 
         self.button_camera.clicked.connect(postition_calculating.loop) 
+        self.button_add_to_path.cliced.connect(self.add_to_path)
         ######################
         
         ##Adding items to postions list##
-        self.add_items_to_list()
+        self.add_items_to_label()
+        
+        ##Append list to comboBox##
+        self.add_items_to_combo()
         
         ##Show window##
         self.show_window()
@@ -36,8 +46,23 @@ class WindowApp:
     def show_window(self):
         self.window.show()
     
+    def add_to_path(self):
+        
+        pass
+    
+    #Get list of num of positions eg. ['1', '2', ...] for comboBox#
+    def get_list_of_pos_numbers(self):
+        data_to_return = list()
+        for index in range(len(postition_calculating.get_positions()) // 5):
+            data_to_return.append(str(index + 1))
+        return data_to_return
+    
+    #Append to comboBox#
+    def add_items_to_combo(self):
+        self.combo_items.addItems(self.get_list_of_pos_numbers())
+    
     #Add postions from positions.txt to list_positions#   
-    def add_items_to_list(self):
+    def add_items_to_label(self):
         positions = postition_calculating.get_positions()
         text_to_post = ""
         oneline_text = ""
